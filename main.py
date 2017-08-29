ffe3b5ea8e7e03e7a64f605ac5a6fc7b672de9a9
PhallanX14 <30751432+PhallanX14@users.noreply.github.com>
2017-08-29 22:35:39 +0530

    Generate Master List

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from nltk.tokenize import RegexpTokenizer
+import datetime
 
 SEARCH_THRESHOLD = 5
 
@@ -42,22 +43,28 @@ def process_subtitles():
     subs = pysrt.open("Friends-6x05 The One with Joey's Porsche.srt")
     subtitles = []
     for i in range(len(subs)):
-        subtitles.append((subs[i].start.to_time(), subs[i].end.to_time(), subs[i].text.replace('\n', ' ')))
+        subtitles.append([subs[i].start.to_time(), subs[i].end.to_time(), subs[i].text.replace('\n', ' ')])
     return subtitles
 
 def process_script():
-    url = 'http://uncutfriendsepisodes.tripod.com/season6/605uncut.htm'
+    url = 'http://www.livesinabox.com/friends/season6/605towjp.htm'
     response = requests.get(url)
     html_doc = response.content
     soup = BeautifulSoup(html_doc, 'html.parser')
     script = []
 
     for i in soup.select("p > b:nth-of-type(1)"):
-        text = i.next_sibling.encode('utf-8')
+        text = ' '
+        for j in i.next_siblings:
+            text += str(j)
+            text += ' '
+        text = re.sub(r'\<[^>]*\>', '', text)
+        text = text.encode('utf-8')
         text = remove_utf_symbols(text).decode('utf-8')
         text = re.sub(r'\([^)]*\)', '', text)
+        text = re.sub(r'\{[^}]*\}', '', text)
         text = text.replace('\r','').replace('\n',' ')
-        script.append((i.text,text.strip()))
+        script.append([i.text[:-1],text.strip()])
 
     return script
 
@@ -95,39 +102,44 @@ def find_next_matching(subs_words,script_words,count_i,count_j):
         count_i+=SEARCH_THRESHOLD
         count_j+=SEARCH_THRESHOLD
 
-def merge(subtitles,script,subs_words,script_words):
+
+def merge(subtitles, script, subs_words, script_words):
     final = []
     count_i = 0
     count_j = 0
     count_ii = 0
     count_jj = 0
     while True:
-        if count_i>len(subs_words) or count_j>len(script_words):
+        if count_i >= len(subs_words):
             break
-        temp = []
-        if subs_words[count_i]==script_words[count_j]:
-            if subtitles[count_ii][2].find(subs_words[count_i]) !=-1:
-                temp.append(subtitles[count_ii][0])
-                temp.append(subtitles[count_ii][1])
-            else:
-                count_ii+=1
-                temp.append(subtitles[count_ii][0])
-                temp.append(subtitles[count_ii][1])
-
-            if script[count_jj][1].find(script_words[count_j]) != -1:
-                temp.append(script[count_jj][0])
-            else:
-                count_jj+=1
-                temp.append(script[count_jj][0])
-
-            temp.append(subs_words[count_i])
-            final.append(temp)
-            count_i+=1
-            count_j+=1
+        temp = [subs_words[count_i]]
+        if subtitles[count_ii][2].find(subs_words[count_i]) != -1:
+            temp.append(subtitles[count_ii][0])
+            temp.append(subtitles[count_ii][1])
         else:
-            count_i,count_j = find_next_matching(subs_words,script_words,count_i,count_j)
-    return final
+            count_ii += 1
+            temp.append(subtitles[count_ii][0])
+            temp.append(subtitles[count_ii][1])
+        count_i += 1
+        final.append(temp)
 
+    count_i = 0
+    while True:
+        if count_i > len(subs_words) or count_j > len(script_words):
+            break
+        if subs_words[count_i].lower() == script_words[count_j].lower():
+            if script[count_jj][1].find(script_words[count_j]) != -1:
+                final[count_i].append(script[count_jj][0])
+            else:
+                count_jj += 1
+                final[count_i].append(script[count_jj][0])
+
+            count_i += 1
+            count_j += 1
+        else:
+            count_i, count_j = find_next_matching(subs_words, script_words, count_i, count_j)
+
+    return final
 def main():
     subtitles = process_subtitles()
     script = process_script()
@@ -136,7 +148,11 @@ def main():
     print(subs_words)
     print(script_words)
     final = merge(subtitles,script,subs_words,script_words)
-    print(final)
+    for i in range(len(final)):
+        if len(final[i])>3:
+            print(final[i][0],final[i][3])
+        else:
+            print(final[i][0])
 
 
 
